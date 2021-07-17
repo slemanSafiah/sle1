e1c874b8b21b444ab8eee173fa65d935c2906f6b
sleman safiah <sleman.safiah@inbuco.me>
2021-07-18 01:12:46 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const errParser = require("error-stack-parser");
+const mongoose = require("mongoose");
+var PrettyError = require("pretty-error");
+const axios = require("axios");
+var pe = new PrettyError();
+pe.appendStyle();
+
+function method(ele) {
+  return (
+    ele.fileName.includes("C:") ||
+    ele.fileName.includes("D:") ||
+    ele.fileName.includes("E:") ||
+    ele.fileName.includes("F:")
+  );
+}
+
+function Sleman({ option, error }) {
+  let err = errParser.parse(error);
+  let errObject = err.filter(method);
+  let col = errObject[0].columnNumber;
+  let row = errObject[0].lineNumber;
+  let filePath = errObject[0].fileName;
+
+  const renderedError = pe.render(error);
+  let arr = renderedError.split("\n");
+  let e = arr[0];
+  e = e.replace(/([\[\]\/\\])/g, "");
+  let str = "";
+  for (let i = 0; i < e.length; i++) {
+    if (
+      (e[i] >= "a" && e[i] <= "z") ||
+      (e[i] >= "A" && e[i] <= "Z") ||
+      (e[i] >= "0" && e[i] <= "9") ||
+      e[i] === "." ||
+      e[i] === " "
+    )
+      str += e[i];
+  }
+  let cut2 = str.indexOf("0m90m0m37m 0m97m");
+  let title = str.substring(10, cut2);
+  let desc = str.substring(cut2 + 16, str.length - 2);
+  // link to asp endpoint
+  const config = {
+    method: "POST",
+    url: "https://projectfifthyear.herokuapp.com/Errors", // add the option to make the api reliable
+    data: {
+      column: parseInt(col),
+      row: parseInt(row),
+      fileName: "from file path",
+      Path: filePath,
+      title: title,
+      description: desc,
+    },
+  };
+  axios(config);
+}
+
+module.exports.sleman = Sleman;
---
+++ readme.md
@@ -0,0 +1,55 @@
+![sleman](https://www.fontmirror.com/app_public/files/t/1/featured_image/2020/06/featured_6888.jpg)
+
+```
+const { sleman } = require("sle1");
+const option = {
+  //some credentials
+};
+try {
+    //some code emmit an error
+} catch (err) {
+  sleman({ option, error: err });
+}
+```
+
+# Installation
+
+This is a NodeJS library can be installed via npm.
+
+Before installing, [download and install Node.js](https://nodejs.org/en/download/). Node.js 0.10 or higher is required
+
+```
+ $ npm install sle1
+```
+
+or
+
+```
+$ npm i sle1
+```
+
+---
+
+# Using
+
+The using of our library is cooperate with [Logger.com](http://localhost:5000).
+
+Use the credentials after you get it from the website as the example above.
+
+---
+
+# Documentation and Code
+
+you can look for the code on github [here](https://github.com) .
+
+---
+
+# People
+
+Sleman Safiah (software engineer) is the author of sle1 .
+
+---
+
+# License
+
+MIT
